94286e4db9b66d29a74b979950a833fa8aea39c1
main <forcourseracourse111@gmail.com>
2021-05-31 19:30:47 +0300

    modified to fit the modul wth GUI

--- password-generator/passwordGenerator.py
+++ password-generator/passwordGenerator.py
@@ -5,29 +5,26 @@ lowercase_letters = appercase_letters.lower()
 digits = "0123456789"
 symbols = "!@#$%^&"
 
-apper, lower, nums, symb = True, True, True, True
+def generate_password(lengh: int, apper=True, lower=True, nums=True, symb=True):
+    char_to_use = ""
+    password = ""
+    
+    if apper:
+        char_to_use += appercase_letters
+    if lower:
+        char_to_use += lowercase_letters
+    if digits:
+        char_to_use += digits
+    if symb:
+        char_to_use += symbols
+    
+    for i in range(lengh):
+        password += char_to_use[int(random.randrange(0,len(char_to_use)))]
+    return password
 
-char_to_use = ""
-lengh_of_password = 20
-amount = 10
-passord_storage = {}
-
-if apper:
-    char_to_use += appercase_letters
-
-if lower:
-    char_to_use += lowercase_letters
-
-if digits:
-    char_to_use += digits
-
-if symb:
-    char_to_use += symbols
-
-
-for p in range(amount):
-    passord_storage[p] = ""
-    for i in range(lengh_of_password):
-        passord_storage[p] += char_to_use[int(random.randrange(0,len(char_to_use)))]
-
-print(passord_storage)
+def to_dictionarey(app_name: str, username: str, password: str):
+    ret = {
+        "app_name": app_name,
+        "username": username,
+        "password": password
+    }
